000b6a6a7da52487c16c6b56d18b5ab4915d6880
Linda Tong <42878194+lindaktong@users.noreply.github.com>
2024-07-09 23:22:26 -0700

    getting all users on curius

---
+++ sorting.py
@@ -0,0 +1,36 @@
+import json
+
+try:
+    # Read the data from a text file
+    with open('allusers.txt', 'r') as file:
+        data = file.read()
+
+    # Convert the string to a dictionary
+    data_dict = json.loads(data)
+
+    # Extract the list of users
+    users_list = data_dict.get('users', [])
+
+    # Sort the list of dictionaries by 'id'
+    sorted_users = sorted(users_list, key=lambda x: x['id'])
+
+    # Update the dictionary with the sorted list
+    data_dict['users'] = sorted_users
+
+    # Write the sorted data to a text file
+    # with open('sorted.txt', 'w') as file:
+    #     file.write(json.dumps(data_dict, indent=2))
+
+    print("Sorted data written to sorted.txt")
+
+except json.JSONDecodeError as e:
+    print(f"Error decoding JSON: {e}")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+# Extract all the ids
+ids = [user['id'] for user in users_list]
+
+# Print the list of ids
+print(sorted(ids))
+print(len(ids))
